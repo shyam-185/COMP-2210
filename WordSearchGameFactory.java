912dde44e087f0bac0f9fa639a611b6967dcd438
shyam-185 <135179716+shyam-185@users.noreply.github.com>
2023-10-12 14:58:45 -0500

    Add files via upload

---
+++ WordSearchGameFactory.java
@@ -0,0 +1,16 @@
+
+/**
+ * Provides a factory method for creating word search games. 
+ */
+public class WordSearchGameFactory {
+
+    /**
+     * Returns an instance of a class that implements the WordSearchGame
+     * interface.
+     */
+    public static WordSearchGame createGame() {
+        ShyamSearch client = new ShyamSearch();
+        return client;
+    }
+
+}
